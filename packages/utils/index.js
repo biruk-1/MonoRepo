9d82b9fe0568e8a987fb8841df7eb2c0d39a00e5
BiileyX <brxsitota@gmail.com>
2026-04-24 17:33:33 +0300

    feat(utils, feature-x): form rules, apiHelper timeout, vitest tests, login validation

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,3 +1,4 @@
 export { formatDate } from "./src/formatDate.js";
 export { apiHelper } from "./src/apiHelper.js";
 export { isValidEmail } from "./src/validation.js";
+export { isNonEmpty, minLength } from "./src/formRules.js";
